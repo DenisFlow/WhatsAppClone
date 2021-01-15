ccac310246f91a80b6ab0ee7fc4f5daf518ea765
DenisFlow <deo9flow@gmail.com>
2021-01-15 22:07:09 +0300

    ready

--- app/src/main/java/com/example/whatsappclone/Chat.java
+++ app/src/main/java/com/example/whatsappclone/Chat.java
@@ -8,12 +8,15 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +27,7 @@ public class Chat extends AppCompatActivity {
     private ArrayList<String> arrayList;
     private ArrayAdapter arrayAdapter;
     private EditText edtMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +45,7 @@ public class Chat extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter(Chat.this, android.R.layout.simple_expandable_list_item_1, arrayList);
 
         ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Chat");
-//        getIntent().getExtras();
-//        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
-//        String[] users = {ParseUser.getCurrentUser().getUsername(), getIntent().getExtras().get("waTargetRecipient").toString()};
+
         ArrayList<String> strCol = new ArrayList<>();
         strCol.add(ParseUser.getCurrentUser().getUsername());
         strCol.add(getIntent().getExtras().get("waTargetRecipient").toString());
@@ -69,6 +71,49 @@ public class Chat extends AppCompatActivity {
         buttonSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (edtMessage.getText().toString() != "") {
+                    ParseObject parseObject = new ParseObject("Chat");
+                    parseObject.put("waSender", ParseUser.getCurrentUser().getUsername());
+                    parseObject.put("waTargetRecipient", getIntent().getExtras().get("waTargetRecipient").toString());
+                    parseObject.put("waMessage", edtMessage.getText().toString());
+
+                    parseObject.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            ArrayList<String> strCol = new ArrayList<>();
+                            strCol.add(ParseUser.getCurrentUser().getUsername());
+                            strCol.add(getIntent().getExtras().get("waTargetRecipient").toString());
+
+                            parseQuery.whereContainedIn("waSender", strCol);
+                            parseQuery.whereContainedIn("waTargetRecipient", strCol);
+                            parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                                @Override
+                                public void done(List<ParseObject> objects, ParseException e) {
+                                    if (e == null){
+                                        if (objects.size() > 0){
+                                            boolean wasChange = false;
+                                            for (ParseObject msg : objects){
+                                                if (!arrayList.contains(msg.getString("waSender") + ": " + msg.getString("waMessage"))) {
+                                                    arrayList.add(msg.getString("waSender") + ": " + msg.getString("waMessage"));
+                                                    wasChange = true;
+
+                                                }
+                                            }
+                                            if (wasChange){
+                                                listChat.deferNotifyDataSetChanged();
+                                            }
+
+                                        }
+                                    }
+
+                                }
+                            });
+
+                        }
+                    });
+                } else {
+                    FancyToast.makeText(Chat.this, "Your message is empty!", FancyToast.LENGTH_LONG, FancyToast.INFO, true).show();
+                }
 
             }
         });
